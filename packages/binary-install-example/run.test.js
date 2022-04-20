2f860bba56199cbd7c6cb4d0aa17b7278f172713
Avery Harnish <averyharnish@gmail.com>
2022-04-20 16:34:21 -0500

    chore: removes unnecessary run.js

--- packages/binary-install-example/run.test.js
+++ packages/binary-install-example/run.test.js
@@ -10,6 +10,10 @@ test("it's installed", () => {
   expect(fs.existsSync(bin)).toBe(true);
 });
 
-test("it can print to stdout and count to 4", () => {
+test("direct bin can print to stdout and count to 4", () => {
   expect(shell.exec(`${bin} -cccc`).stdout).toContain("4");
 });
+
+test("npx can print to stdout and count to 9", () => {
+  expect(shell.exec("npx binary-install-example -ccccccccc").stdout).toContain("9");
+})
